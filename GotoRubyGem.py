dafba6fe50f6c0353306d5ffa8f5f918832a2dda
Zain Iftikhar <zain.iftikhar@7vals.com>
2024-05-09 00:03:31 +0500

    Provided option to user to select Gems from custom directories

--- GotoRubyGem.py
+++ GotoRubyGem.py
@@ -2,8 +2,14 @@ import sublime
 import sublime_plugin
 import os
 
+SETTINGS_FILE = "GotoRubyGem.sublime-settings"
+
 class GotoRubyGemCommand(sublime_plugin.WindowCommand):
   def run(self, gem):
+    # If Gems directories are empty then early return.
+    if not gem:
+      return
+
     sublime.run_command('new_window')
     new_window = sublime.active_window()
     project_data = { "folders": [{"path": gem}] }
@@ -15,12 +21,17 @@ class GotoRubyGemCommand(sublime_plugin.WindowCommand):
 class GemInputHandler(sublime_plugin.ListInputHandler):
 
   def list_items(self):
-    gem_home = os.popen("echo $GEM_HOME").read().split("\n")[0]
+    settings = sublime.load_settings(SETTINGS_FILE)
+    use_custom_gems_directories = settings.get("use_custom_gems_directories", False)
 
-    gems_directories = [
-      os.path.expanduser("%s/gems/" % gem_home),
-      os.path.expanduser("%s/bundler/gems/" % gem_home)
-    ]
+    if use_custom_gems_directories:
+      gems_directories = settings.get("custom_gems_directories", [])
+    else:
+      gem_home = os.popen("echo $GEM_HOME").read().split("\n")[0]
+      gems_directories = [
+        os.path.expanduser("%s/gems/" % gem_home),
+        os.path.expanduser("%s/bundler/gems/" % gem_home)
+      ]
 
     # Create a list to store gem names and their absolute paths
     gems_list = []
